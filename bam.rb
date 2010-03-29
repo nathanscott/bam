445046b3569d55d9ec3b0d929d112639fd650afd
Ben Hoskings <ben@hoskings.net>
2010-03-29 18:31:30 +1100

    Added CoffeeScript support.

--- bam.rb
+++ bam.rb
@@ -1,7 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'coffee-script'
 
+get %r{(.*)\.js$} do |basename|
+  content_type :js
+  CoffeeScript.compile File.read(File.join options.views, "#{basename}.coffee")
+end
 
 get '/' do
   haml :index
