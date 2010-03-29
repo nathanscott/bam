a0b279c0d1e2f4d3289633745666bbdbf3dbc769
nathanscott <nathan@inspire9.com.au>
2010-03-29 19:04:18 +1100

    setup outlined

--- bam.rb
+++ bam.rb
@@ -11,3 +11,15 @@ end
 get '/' do
   haml :index
 end
+
+get '/preview' do
+  haml params[:style].to_sym
+end
+
+get '/save' do
+  # save via ajax and return the url string
+end
+
+get '/help' do
+  # redirect or redirect to another
+end
